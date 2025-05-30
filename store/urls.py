78703064d16ef14d30c6fff7c623220d5a29a89b
LoreFedi28 <fedilorenzogm@gmail.com>
2025-05-31 01:33:32 +0200

    added customer and manager

--- store/urls.py
+++ store/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ProductListView
+from .views import ProductListView, ManagerOnlyView
 
 urlpatterns = [
     path('products/', ProductListView.as_view(), name='product-list'),
+    path('manager/', ManagerOnlyView.as_view(), name='manager-only'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,7 +1,17 @@
 from django.views.generic import ListView
+from django.views.generic import TemplateView
+from django.contrib.auth.mixins import UserPassesTestMixin
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.utils.decorators import method_decorator
 from .models import Product
 
 class ProductListView(ListView):
     model = Product
     template_name = 'store/product_list.html'
     context_object_name = 'products'
+
+class ManagerOnlyView(UserPassesTestMixin, TemplateView):
+    template_name = 'store/manager_only.html'
+
+    def test_func(self):
+        return self.request.user.groups.filter(name='Manager').exists()
